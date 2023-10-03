8f0b791658da544c30440d18ffac933eec56b0be
Rishabh_Kumar2201 <rishabhkumar2204@gmail.com>
2023-10-04 00:15:29 +0530

    Added new Codes

--- lib/Activity/home.dart
+++ lib/Activity/home.dart
@@ -10,7 +10,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  SearchController searchController = new SearchController();
+  SearchController searchController = SearchController();
   int counter = 0;
 
   @override
@@ -32,143 +32,213 @@ class _HomeState extends State<Home> {
     Object? info = ModalRoute.of(context)?.settings.arguments;
 
     return Scaffold(
-        body: SafeArea(
-            child: Container(
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-              colors: [Colors.pinkAccent, Colors.lightGreenAccent],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight)),
-      child: Column(
-        children: [
-          Container(
-            //Search Wala Container
-
-            padding: EdgeInsets.symmetric(horizontal: 8),
-            margin: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+        appBar: AppBar(
+          toolbarHeight: 0,
+          flexibleSpace: Container(
             decoration: BoxDecoration(
-                color: Colors.white, borderRadius: BorderRadius.circular(24)),
-            child: Row(
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    if ((searchController.text).replaceAll(" ", "") == "") {
-                      print("Blank Search");
-                    } else {}
-                  },
-                  child: Container(
-                    child: Icon(
-                      Icons.search,
-                      color: Colors.blueAccent,
-                    ),
-                    margin: EdgeInsets.fromLTRB(3, 0, 7, 0),
-                  ),
-                ),
-                Expanded(
-                  child: TextField(
-                    controller: searchController,
-                    textInputAction: TextInputAction.search,
-                    decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Search Your City Name !!"),
-                  ),
-                )
-              ],
+              gradient: LinearGradient(
+                colors: [
+                  Colors.greenAccent,
+                  Colors.lightGreen,
+                ], // Replace with your gradient colors
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
             ),
           ),
-          Row(
-            children: [
-              Expanded(
-                child: Container(
-                  margin: EdgeInsets.symmetric(horizontal: 25),
-                  padding: EdgeInsets.all(27),
+        ),
+        body: SingleChildScrollView(
+          child: SafeArea(
+              child: Container(
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [Colors.pinkAccent, Colors.lightGreenAccent],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight)),
+            child: Column(
+              children: [
+                Container(
+                  //Search Wala Container
+
+                  padding: EdgeInsets.symmetric(horizontal: 8),
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 24, vertical: 20),
                   decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.white.withOpacity(0.5),
-                  ),
-                  child: Text(
-                    "This is another container",
-                    style: TextStyle(
-                      fontSize: 15,
-                    ),
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(24)),
+                  child: Row(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          if ((searchController.text).replaceAll(" ", "") ==
+                              "") {
+                            print("Blank Search");
+                          } else {}
+                        },
+                        child: Container(
+                          child: Icon(
+                            Icons.search,
+                            color: Colors.blueAccent,
+                          ),
+                          margin: EdgeInsets.fromLTRB(3, 0, 7, 0),
+                        ),
+                      ),
+                      Expanded(
+                        child: TextField(
+                          controller: searchController,
+                          textInputAction: TextInputAction.search,
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Search Your City Name !!"),
+                        ),
+                      )
+                    ],
                   ),
                 ),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Expanded(
-                child: Container(
-                  height: 300,
-                  margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
-                  padding: EdgeInsets.all(15),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.white.withOpacity(0.5),
-                  ),
-                  child: Text(
-                    "This is another container",
-                    style: TextStyle(
-                      fontSize: 15,
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                          margin: EdgeInsets.symmetric(horizontal: 25),
+                          padding: EdgeInsets.all(27),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.white.withOpacity(0.5),
+                          ),
+                          child: Row(
+                            children: [
+                              Text("Network Imageadd:url"),
+                              Column(
+                                children: [
+                                  Text(
+                                    "Scattered Clouds",
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    "in Ballia",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 15),
+                                  )
+                                ],
+                              ),
+                            ],
+                          )),
                     ),
-                  ),
+                  ],
                 ),
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Expanded(
-                child: Container(
-                  height: 200,
-                  //width: 150,
-                  margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
-                  padding: EdgeInsets.all(15),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.white.withOpacity(0.5),
-                  ),
-                  child: Text(
-                    "Hello",
-                    style: TextStyle(
-                      fontSize: 15,
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                          height: 300,
+                          margin: EdgeInsets.symmetric(
+                              horizontal: 25, vertical: 10),
+                          padding: EdgeInsets.all(15),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.white.withOpacity(0.5),
+                          ),
+
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: Container(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Image.asset("assets/thermo.png",
+                                    height: 110,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: Container(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text("36.7",
+                                    style: TextStyle(
+                                      fontSize: 60,
+                                      fontWeight: FontWeight.bold
+                                    ),),
+                                    Text("   \u00B0C",
+                                      style: TextStyle(
+                                          fontSize: 40
+                                      ),)
+                                  ],
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+
+                    //Text("Temp11")
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Expanded(
+                      child: Container(
+                        height: 200,
+                        //width: 150,
+                        margin:
+                            EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+                        padding: EdgeInsets.all(15),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.white.withOpacity(0.5),
+                        ),
+                        child: Text(
+                          "Hello",
+                          style: TextStyle(
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  height: 200,
-                  //width: 150,
-                  margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
-                  padding: EdgeInsets.all(15),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.white.withOpacity(0.5),
-                  ),
-                  child: Text(
-                    "container",
-                    style: TextStyle(
-                      fontSize: 15,
+                    Expanded(
+                      child: Container(
+                        height: 200,
+                        //width: 150,
+                        margin:
+                            EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+                        padding: EdgeInsets.all(15),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.white.withOpacity(0.5),
+                        ),
+                        child: Text(
+                          "container",
+                          style: TextStyle(
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
                     ),
-                  ),
+                  ],
                 ),
-              ),
-            ],
-          ),
-          SizedBox(height: 20),
-          Text(
-            "Made With 💖 By Rishabh",
-            style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
-          ),
-          Text(
-            "Referenced from CodeWithDhruv",
-            style: TextStyle(fontSize: 11),
-          ),
-        ],
-      ),
-    )));
+                SizedBox(height: 20),
+                Text(
+                  "Made With 💖 By Rishabh",
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+                ),
+                Text(
+                  "Referenced from CodeWithDhruv",
+                  style: TextStyle(fontSize: 11),
+                ),
+    ],
+            ),
+          )),
+        ));
   }
 }
